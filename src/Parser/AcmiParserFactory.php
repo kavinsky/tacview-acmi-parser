880dd6f16546a1d7d18ab86649f388950d678337
Ignacio Muñoz Fernandez <nmunozfernandez@gmail.com>
2021-10-14 18:47:16 +0200

    Done with AcmiParserFactory

--- src/Parser/AcmiParserFactory.php
+++ src/Parser/AcmiParserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Kavinsky\TacviewAcmiReader\Parser;
 
+use Kavinsky\TacviewAcmiReader\Acmi;
 use Kavinsky\TacviewAcmiReader\Parser\Reader\AcmiReaderInterface;
 use Kavinsky\TacviewAcmiReader\Parser\Reader\AcmiTextReader;
 use Kavinsky\TacviewAcmiReader\Parser\Reader\AcmiZipReader;
@@ -36,6 +37,20 @@ class AcmiParserFactory
         }
 
         $this->setHandlers($handlers);
+    }
+
+
+    /**
+     * @param string $filePath
+     * @return Acmi
+     * @throws \Exception
+     */
+    public function parse(string $filePath): Acmi
+    {
+        $reader = $this->getSupportedReader($filePath);
+
+        return (new AcmiParser($reader, $this->handlers))
+            ->parseFromFile($filePath);
     }
 
     /**
@@ -85,6 +100,12 @@ class AcmiParserFactory
         return $this;
     }
 
+    /**
+     * Appends a sentence handler to the registry
+     *
+     * @param SentenceHandlerInterface $handler
+     * @return $this
+     */
     public function addHandler(SentenceHandlerInterface $handler): self
     {
         array_push($this->handlers, $handler);
@@ -92,6 +113,12 @@ class AcmiParserFactory
         return $this;
     }
 
+    /**
+     * Sets the Handler library
+     *
+     * @param SentenceHandlerInterface|array<SentenceHandlerInterface> $handlers
+     * @return $this
+     */
     public function setHandlers(SentenceHandlerInterface|array $handlers): self
     {
         if (! is_array($handlers)) {
@@ -101,5 +128,23 @@ class AcmiParserFactory
         $this->handlers = $handlers;
 
         return $this;
+    }
+
+    /**
+     * Gets a supported by the filePath acmi reader.
+     *
+     * @param string $filePath
+     * @return AcmiReaderInterface
+     * @throws \Exception
+     */
+    protected function getSupportedReader(string $filePath): AcmiReaderInterface
+    {
+        foreach ($this->readers as $reader) {
+            if ($reader->supports($filePath)) {
+                return $reader;
+            }
+        }
+
+        throw new \Exception('No supported readers available to read file: '.basename($filePath));
     }
 }
